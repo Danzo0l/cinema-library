df79de991bec0bd76c9e6fed528efd4416245e1b
danzo0l <dryagindenisrus@gmial.com>
2022-02-22 15:01:51 -0800

    lesson 17 -end

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
@@ -7,16 +8,25 @@ from . import forms
 
 
 # Create your views here.
-class MoviesView(ListView):
+class GenreYear:
+
+    def get_genres(self):
+        return models.Genre.objects.all()
+
+    def get_years(self):
+        return models.Movie.objects.filter(draft=False).values('year')
+
+
+class MoviesView(GenreYear, ListView):
     '''Films list'''
     
     model = models.Movie
     queryset = models.Movie.objects.filter(draft=False)
-    # template_name = 'movies/movie_list.html'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     '''full movie description'''
+
 
     model = models.Movie
     slug_field = 'url'
@@ -35,3 +45,21 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(GenreYear, DetailView):
+    model = models.Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
+
+
+class FilterMovieView(GenreYear, ListView):
+
+    def get_queryset(self):
+        queryset = models.Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        print(queryset)
+        return queryset
+
